f8a5798fb5d27caa3184f4beb00a0e30a063946d
jhony <jhony.taveira@hotmail.com>
2014-08-19 21:20:41 -0300

    file ruby version 1.8.5

---
+++ clicktocal.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'mysql'
+
+user = "root"
+db = "clicktocal_development"
+host = "localhost"
+pass = ""
+con = Mysql.real_connect(host, user, pass, db)
+
+query = con.query("select id,ddd,number,carried_out from register_numbers where carried_out=0")
+query.each do |q|
+  update = con.query("update register_numbers set carried_out = 1 where id = #{q[0]}")
+
+  path = "/home/jhony/#{q[1, 2]}.call"
+  File.open(path, "w+") do |f|
+    f.write("Channel: Local/#{q[1, 2]}@mor
+      MaxRetries: 1
+      RetryTime: 120
+      WaitTime: 30
+      Account: 8
+      Context: mor
+      Application: Queue
+      Data: problema_tecnico
+      Extension: #{q[1, 2]}
+      Priority: 1
+      Set: MOR_AD_CAMPAIGN_ID=3")
+  end
+
+
+end
+              
+              
+        
+
+
+
+
+
+		
+
